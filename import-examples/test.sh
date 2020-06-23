b2ecd72afaa3805c248894551fc6c53e3f90e44a
reggi <thomas@reggi.com>
2020-06-22 21:34:59 -0400

    better toolign

--- import-examples/test.sh
+++ import-examples/test.sh
@@ -1,17 +1,24 @@
-node ./js-no-pkg/index.js && 
+#!/bin/bash 
+set -e 
+set -o pipefail
+
+find . -name "node_modules" -exec rm -rf '{}' +
+# find . -name 'package-lock.json' -type f -delete
+
+node ./js-no-pkg/index.js
 echo "js-no-pkg ✅"
 
-npm -C ./js-pkg-tilde -s install && node ./js-pkg-tilde/index.js &&
+npm -C ./js-pkg-tilde -s install && node ./js-pkg-tilde/index.js
 echo "js-pkg-tilde ✅"
 
-npm -C ./js-pkg-tilde-nested -s install && node ./js-pkg-tilde-nested/index.js &&
+npm -C ./js-pkg-tilde-nested -s install && node ./js-pkg-tilde-nested/index.js
 echo "js-pkg-tilde-nested ✅"
 
-node --experimental-modules ./mjs-no-pkg/index.mjs &&
+node --experimental-modules ./mjs-no-pkg/index.mjs
 echo "mjs-no-pkg ✅"
 
-npm -C ./mjs-pkg-tilde -s install && node --experimental-modules ./mjs-pkg-tilde/index.mjs &&
+npm -C ./mjs-pkg-tilde -s install && node --experimental-modules ./mjs-pkg-tilde/index.mjs
 echo "mjs-pkg-tilde ✅"
 
-npm -C ./mjs-pkg-tilde-nested -s install && node --experimental-modules ./mjs-pkg-tilde-nested/index.mjs &&
+npm -C ./mjs-pkg-tilde-nested -s install && node --experimental-modules ./mjs-pkg-tilde-nested/index.mjs
 echo "mjs-pkg-tilde-nested ✅"
